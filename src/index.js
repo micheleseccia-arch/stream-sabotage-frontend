2d6f2fefe7963a386ffab67e900dcdcca02c1591
micheleseccia-arch <michele.seccia@cassandroferminervi.edu.it>
2026-05-22 11:04:00 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
 import { GameRoom } from './GameRoom';
-
+const SERVER_URL = 'https://stream-sabotage.onrender.com';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
