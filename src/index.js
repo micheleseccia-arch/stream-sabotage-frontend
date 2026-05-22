9cc5450b33b387c5f8f54c0496c4b540e4cb7b2d
micheleseccia-arch <michele.seccia@cassandroferminervi.edu.it>
2026-05-22 11:03:20 +0200

    index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.css';
+import { GameRoom } from './GameRoom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <GameRoom />
+  </React.StrictMode>
+);
